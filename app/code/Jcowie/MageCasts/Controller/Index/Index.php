a286725b22c2eecc2a4175f8ad92160847046122
jamescowie <james@jcowie.co.uk>
2015-07-13 22:11:02 +0100

    Add helper and inject into the controller

--- app/code/Jcowie/MageCasts/Controller/Index/Index.php
+++ app/code/Jcowie/MageCasts/Controller/Index/Index.php
@@ -4,6 +4,7 @@ namespace Jcowie\MageCasts\Controller\Index;
 
 use Magento\Framework\App\Action\Context;
 use Jcowie\MageCasts\Model\CmsPages;
+use Jcowie\MageCasts\Helper\Data;
 
 
 class Index extends \Magento\Framework\App\Action\Action
@@ -11,14 +12,20 @@ class Index extends \Magento\Framework\App\Action\Action
     /** @var  \Jcowie\MageCasts\Model\CmsPages */
     private $cmsPages;
 
-    public function __construct(Context $context, CmsPages $cmsPages)
+    /** @var \Jcowie\MageCasts\Helper\Data  */
+    private $helper;
+
+    public function __construct(Context $context, CmsPages $cmsPages, Data $helper)
     {
         $this->cmsPages = $cmsPages;
+        $this->helper = $helper;
         parent::__construct($context);
     }
 
     public function execute()
     {
+        $this->helper->welcomeHelper();
+
         $pages = $this->cmsPages->getAllPages();
 
         $this->getResponse()->setHeader('content-type', 'text/plain');
---
+++ app/code/Jcowie/MageCasts/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jcowie\MageCasts\Helper;
+
+use Magento\Framework\App\Action\Action;
+
+class Data
+{
+    public function welcomeHelper()
+    {
+        return "Welcome from the helper";
+    }
+}
